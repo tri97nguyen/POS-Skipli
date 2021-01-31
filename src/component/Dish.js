c687aae614a1eaf07225ed54c7b95573441fd3c4
TriNguyen <trungminhtri.nguyen@my.ccsu.edu>
2021-01-31 00:48:40 -0500

    working on selecting dish

---
+++ src/component/Dish.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Dish() {
+    return (
+        <div className="dish">
+            <div className="quantity">1</div>
+        </div>
+    )
+}
--- src/component/Menu.js
+++ src/component/Menu.js
@@ -2,10 +2,12 @@ import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import { toggleOrderPanelFromMenu } from '../shared/service'
+import Dish from './Dish'
+
 export default function Menu() {
     var photos = []
     for (var i = 0; i < 100; i++) {
-        photos.push(<div className="image" key={i} alt="some thing" >dish image</div>)
+        photos.push(<Dish />)
     }
     return (
         <section id="menu" onClick={toggleOrderPanelFromMenu} className="menu grid-column-row">
