ae1b901313d30ba327b21985832027536e66908e
TriNguyen <trungminhtri.nguyen@my.ccsu.edu>
2021-01-30 19:39:58 -0500

    implemented modal to pick table

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,15 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from './Modal'
 
 export default function Footer() {
+    var [modalOpen, setModalOpen] = useState(false);
+
+    function toggleTable(e) {
+        e.stopPropagation();
+        setModalOpen(!modalOpen);
+
+    }
     return (
         <div className="footer">
             <div>footer</div>
             <button className="btn table-btn" onClick={toggleTable}>Table</button>
+            {modalOpen ?
+                <Modal>
+                    <div onClick={toggleTable} className="modal-backdrop">
+                        <div onClick={e => e.stopPropagation()} className="modal-content">
+                            <div className="modal-header">
+                                <div>Choose Table</div>
+                                <div onClick={toggleTable} id="modal-close-btn" className="close-btn">&times;</div>
+                            </div>
+                        </div>
+                    </div>
+                </Modal>
+                : <></>
+            }
         </div>
     )
 }
 
-function toggleTable(e) {
-    e.stopPropagation();
-    
-}
