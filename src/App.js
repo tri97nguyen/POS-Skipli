6941a0abc34a93d3555d7f628404ebc4dc9ae65f
TriNguyen <trungminhtri.nguyen@my.ccsu.edu>
2021-02-02 01:10:21 -0500

    set up deployment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 import Login from './component/Login'
-import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, withRouter, Redirect } from 'react-router-dom'
 import UserContext from './providers/User'
 import Dashboard from './component/Dashboard';
 
@@ -12,6 +12,7 @@ export default function App() {
     <UserContext>
       <Route path="/dashboard" component={Dashboard} />
       <Route path="/login" component={Login} />
+      <Redirect from="*" to="/login" />
     </UserContext>
 
 
